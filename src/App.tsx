c33a6b1daf0b0624ef241827129562a6732cdba4
redaipoo <redaoian@gmail.com>
2026-09-01 19:10:29 +0200

    ci: configure GitHub Pages hosting and Actions automated deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { FavoritesProvider } from './context/FavoritesContext';
 import { ScrollToTop } from './components/layout/ScrollToTop';
 import { Navbar } from './components/layout/Navbar';
